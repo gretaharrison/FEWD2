7469f5c1eff671ba03dc2bb0fb45953a5e0b4505
gretaharrison <gretarrison@gmail.com>
2015-07-20 21:59:20 -0700

    7/20/2015 9:59PM

--- passport_beauty/js/app.js
+++ passport_beauty/js/app.js
@@ -1,5 +1,12 @@
 var slider = document.getElementById('slider');
 slider.Slider();
 
-var contact = document.getElementById('contact');
-contact.Contact();
+//var contact = document.getElementById('contact');
+//contact.Contact();
+
+var mapobj = document.getElementById('map2').getSVGDocument();
+
+//console.log(map);
+//mapobj.getElementById('mexico').addEventListener('click', function() {
+  //alert("hola");
+//});
--- passport_beauty/js/slider.js
+++ passport_beauty/js/slider.js
@@ -11,10 +11,8 @@ this.closeSlider = function() {
   this.init = function() {
     slider.className='initial';
     this.closeSlider();
-    document.getElementById('mexico').addEventListener('click', function() {
-      alert("hola");
-    });
-    console.log(this);
+
+  //  console.log(this);
   }
 
 
