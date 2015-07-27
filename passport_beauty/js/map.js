b17b10255be8ba3ed740bb6a60d103406ae01046
gretaharrison <gretarrison@gmail.com>
2015-07-27 15:10:04 -0700

    7/27/2015 3:10pm

--- passport_beauty/js/map.js
+++ passport_beauty/js/map.js
@@ -14,7 +14,24 @@ Element.prototype.Map = function() {
       for(var j=0; j<countries[i].length; j++)(function(j){
         var country = countries[i][j];
         country.addEventListener('click', function(){
-          alert('hello from '+ country.id);
+          // alert('hello from '+ country.id);
+
+          var modal = document.getElementById('modal');
+          modal.style.visibility = "visible";
+          var p = document.createElement('p');
+          p.innerHTML = "hello from " + country.id;
+          modal.appendChild(p);
+
+          var close = document.getElementById('close');
+          close.addEventListener('click', function(){
+            modal.style.visibility = "hidden";
+            document.getElementById('p');
+            p.innerHTML = '';
+          });
+
+
+
+
           // var p = document.createElement('p'); // this is where the modal begins
           // p.innerHTML = "hello from "+ country.id;
           // var modal =  document.getElementById('modal');
