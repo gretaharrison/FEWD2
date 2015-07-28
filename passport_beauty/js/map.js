adc87d510de647f65caaae9ad93512ef7366ef47
gretaharrison <gretarrison@gmail.com>
2015-07-27 17:40:02 -0700

    7/27/2015 5:39pm

--- passport_beauty/js/map.js
+++ passport_beauty/js/map.js
@@ -8,7 +8,26 @@ Element.prototype.Map = function() {
     var countries = svg.selectAll("path");
     console.log(countries);
 
-    //var myCountries = [];
+//Loading the JSON
+
+this.connect = function () {
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", "./models/countries.json", true);
+  xhr.setRequestHeader("Content-Type", "application/json");
+
+  xhr.onreadystatechange = function () {
+    if (xhr.readystate == 4) {
+      var response = JSON.parse(xhr.responseText);
+      displayName = response.displayName;
+    }
+  }
+  xhr.send();
+};
+
+
+
+
+// CREATING THE POP UP BOX
 
     for(var i=0; i<countries.length; i++)(function(i){
       for(var j=0; j<countries[i].length; j++)(function(j){
@@ -18,6 +37,11 @@ Element.prototype.Map = function() {
 
           var modal = document.getElementById('modal');
           modal.style.visibility = "visible";
+
+          // var xhr = new XMLHttpRequest();
+          // xhr.open("GET", "./models/countries.json", true);
+          // xhr.setRequestHeader("Content-Type", "application/json");
+
           var p = document.createElement('p');
           p.innerHTML = "hello from " + country.id;
           modal.appendChild(p);
@@ -28,9 +52,6 @@ Element.prototype.Map = function() {
             document.getElementById('p');
             p.innerHTML = '';
           });
-
-
-
 
           // var p = document.createElement('p'); // this is where the modal begins
           // p.innerHTML = "hello from "+ country.id;
